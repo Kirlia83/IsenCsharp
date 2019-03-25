7f857570c71420e6b54a733a443759d1a74d0089
Camille Arbousset <camille.arbousset@isen.yncrea.fr>
2019-03-25 10:40:12 +0100

    create first class hello

---
+++ src/Isen.DotNet.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Isen.DotNet.Library;
+namespace Isen.DotNet.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = new Hello("Camille");
+            var message = hello.Greet();
+            Console.WriteLine(message);
+        }
+    }
+}
